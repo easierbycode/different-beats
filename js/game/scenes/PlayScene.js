e6445d2387cbfe66f0a804c06718c8bfc993ae8a
EasierByCode[43] <daniel@community43.org>
2021-10-28 18:01:19 -0700

    sync

--- js/game/scenes/PlayScene.js
+++ js/game/scenes/PlayScene.js
@@ -10,18 +10,34 @@ class PlayScene extends Phaser.Scene {
     ? '/'
     : '/different-beats/';
     
+    // this.load.spritesheet(
+    //     'lips',
+    //     BASE_URL + 'js/game/assets/lips.png',
+    //     {
+    //         frameWidth: 500,
+    //         frameHeight: 270
+    //     }
+    //   );
+
+    //   this.load.spritesheet(
+    //     'lips',
+    //     BASE_URL + 'js/game/assets/hostage-girl-back.png',
+    //     {
+    //         frameWidth: 30,
+    //         frameHeight: 61
+    //     }
+    // );
+
     this.load.spritesheet(
-        'lips',
-        BASE_URL + 'js/game/assets/lips.png',
-        {
-            frameWidth: 500,
-            frameHeight: 270
-        }
-      );
+        'lips', 
+        BASE_URL + 'js/game/assets/ufo.png', {
+        frameWidth: 32,
+        frameHeight: 30
+    });
   }
 
   create () {
-    const lips = this.physics.add.sprite(400, 200, 'lips', 0)
+    const lips = this.physics.add.sprite(400, 200, 'lips', 0).setScale( 4 )
     lips.anims.create({
       key: 'default',
       frames: lips.anims.generateFrameNumbers('lips'),
